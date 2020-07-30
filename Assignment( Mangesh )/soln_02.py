da098f6a3e804227bc65b2a58ab1af2f0a227372
Abhishek Choudhury <choudhuryabhishek76@gmail.com>
2020-07-30 23:44:41 +0530

    First Commit

---
+++ Assignment( Mangesh )/soln_02.py
@@ -0,0 +1,11 @@
+employee = {
+    "Name":"Ajay",
+    "age":41,
+    "Sex":"M"
+    }
+
+print("The dict is : \n"+str(employee))
+
+employee["City"]="Pune"
+
+print("\nThe dict after update: \n"+str(employee))
---
+++ Assignment( Mangesh )/soln_04.py
@@ -0,0 +1,16 @@
+Scores={
+    "Dhoni":57,
+    "rohit":107,
+    "virat":97,
+    "Sachin":99
+    }
+i=(input("Enter the key : \n"))
+for j in Scores:
+    if(i in Scores):
+        print("The key "+(i)+" exixts")
+        break
+    else:
+        print("No the key doesnt exixst")
+        break
+        
+    
