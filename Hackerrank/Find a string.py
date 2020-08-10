3d4f6ab661ce78413a21652f009f0e61d44c6db2
Abhishek Choudhury <choudhuryabhishek76@gmail.com>
2020-08-10 21:27:47 +0530

    10th Aug 2020

--- Hackerrank/Find a string.py
+++ Hackerrank/Find a string.py
@@ -1,7 +1,23 @@
-n = int(input())
-val = set()
-for i in range(0,n):
-    val.add(input())
+def count_substring(string, sub_string):
+    count = 0
 
-print(val)
-print(len(val))
+    for i in range(0, len(string)-len(sub_string)+1):
+        l = i
+        for j in range(0, len(sub_string)):
+            if string[l] == sub_string[j]:
+                l +=1
+                if j == len(sub_string)-1:
+                    count = count + 1
+                else:
+                    continue
+            else:
+                break
+    return count
+
+    
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
